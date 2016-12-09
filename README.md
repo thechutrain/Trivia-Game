d0cc941b7fa4f79f4bd1204639b4f02f45dd84c2
thechutrain <chu.chuyalan@gmail.com>
2016-12-09 12:41:40 -0500

    beta version of the landing page

--- README.md
+++ README.md
@@ -10,20 +10,21 @@ A basic todo list of things I can keep adding to see what I need to do to make m
 Organized into big tasks (high & low priority) and smaller tasks (high & low priority)
 
 ## Big Rocks
-  #### High Priority ####
-      1. create a landing page, user selects a button that then begins the game
-      2. create a button that allows the user to move to next question after answer
-  #### Lower Priority ####
-      1. randomize the order in which the questions come in
-      2. Keep track of more specific data on user
-        .. * what questions the user got wrong
-        .. * how long it took for them to answer
-        .. * load user data from localStorage?
+#### High Priority ####
+  + 1. create a landing page, user selects a button that then begins the game
+  + 2. create a button that allows the user to move to next question after answer
+#### Lower Priority ####
+  + 1. randomize the order in which the questions come in
+  + 2. Keep track of more specific data on user
+    * what questions the user got wrong
+    * how long it took for them to answer
+    * load user data from localStorage?
 
 
 ## Small Rocks
 ##### High Priority #####
   1. Style the footer & include some Google Fonts
+  2. Keep track of Questions remaining, total correct & incorrect in progress bar
 #### Lower Priority ####
   1. find some nice colors to use
   2. find some funny Gifs to use & include them
---
+++ assets/javascript/game.js
@@ -0,0 +1,195 @@
+/* game.js
+* contains all the code to run the code
+*/
+
+// ----- Main functionality -----
+var Game = {
+  intervalTimer: null, // will hold a reference to the Interval timer
+  countdownTimer: null, //will hold a reference to the countdown 1min
+  currentIndex: 0,
+  secondsLeft: 10,
+  SECONDS_PER_QUESTION: 10,
+  questions_data: TriviaData, // defined globally
+  user_guess: -1,
+  correctAnswers: 0, //
+  incorrectAnswers: 0, // keeps track of total incorrect
+  currentQuestion: 0, // keeps track of the question number
+  totalQuestions: 0, // keeps track of total # of questions
+
+
+  /* initializes the game, gets called in the read() event listener
+  */
+  initialize: function(){
+    this.totalQuestions = this.questions_data.length; // initialize property, error if done above
+    // console.log("totalQuestions: " + this.totalQuestions);
+    // 1) Reset Game!
+    this.incorrectAnswers = 0;
+    this.correctAnswers = 0;
+    this.currentQuestion = 0;
+    $("#update-target").empty();
+    this.currentIndex = 0;
+    this.secondsLeft = this.SECONDS_PER_QUESTION;
+    $('#seconds').html(this.secondsLeft); // prevents the lag in seconds
+    console.log("starting game");
+    this.nextQuestion(this.currentIndex);
+    // console.log("Game is done"); // gets called right after prev line, bc ...
+  },
+
+  /* this method will present the question & answer for display
+  * to the user.
+  * @param index - the index of the question to present
+  */
+  nextQuestion: function(index){
+    // console.log("next quesiton called!");
+    // 0) update the progress bar
+    this.currentQuestion ++;
+    var progress = (this.currentQuestion / this.totalQuestions) * 100; // need as %
+    progress = "width: " + progress.toString() + "%";
+    // console.log(progress);
+    $(".progress-bar").attr("style", progress);
+    // 1) check to see if the index is not in the question data array
+    if (index >= this.questions_data.length){
+      this.gameOver();
+      return;
+    }
+
+  // 2) display the question and answers (event listeners)
+    this.user_guess = -1;
+    this.secondsLeft = this.SECONDS_PER_QUESTION;
+    $('#seconds').html(this.secondsLeft);
+    var question_obj = this.questions_data[index]; // gets current question obj
+    // console.log(question);
+    // A) Get the question
+      var question = $("<h3>").html(question_obj.question);
+      $("#update-target").append(question);
+    // B) Loop through the answerChoices array
+    question_obj.answerChoices.forEach(function(choice, index){
+        // console.log(choice);
+        // console.log("inside the for each" + index);
+        // create a button nested in a div, that displays answer choice with index
+        var button_choice = $("<button>")
+                              .addClass("btn btn-primary btn-block " +
+                               "btn-answerChoice")
+                              .attr("index", index)
+                              .html(choice);
+        var button_wrapper = $("<div>", {'class': 'button-wrapper'})
+                              .append( button_choice );
+        $("#update-target").append(button_wrapper);
+    });
+
+    // 3) write event listener for each button:
+    var self = this;
+    $(".btn-answerChoice").on('click', function(){
+      console.log($(this).attr("index"));
+      // get the user's guess
+      self.user_guess = $(this).attr("index"); // if you use this, it'll be the button, better way??
+      // clear the intervalTimer
+      clearInterval(self.intervalTimer);
+      // reset seconds left
+      self.secondsLeft = self.SECONDS_PER_QUESTION;
+      // empty the div
+      $("#update-target").empty();
+      // show the answer;
+      self.showAnswer();
+    })
+
+  // 4) count down every second
+    this.intervalTimer = setInterval(function(){
+      // console.log(this); // since anonymous func inside setInterval is bound, this is game object
+      // subtract one second
+      this.secondsLeft --;
+      // console.log(this.secondsLeft);
+      $('#seconds').html(this.secondsLeft);
+      // check to make sure that the time is not zero!
+      if (this.secondsLeft <= 0){
+        // clear Interval & reset the count down
+        clearInterval(this.intervalTimer);
+        this.secondsLeft = this.SECONDS_PER_QUESTION;
+        // empty the html div
+        $("#update-target").empty();
+        // show the answer;
+        this.showAnswer();
+      }
+    }.bind(this), 1000); // binds the anonymous func to the game object.
+
+  },
+
+  showAnswer: function(){
+    // 1) Reset timer & empty the div?
+    // Change the view of the timer
+    $('#seconds').html("--");
+
+    // 2) Get the current question & correct answer!
+    var current_question = this.questions_data[this.currentIndex];
+    var correctIndex = current_question.correctAnswer;
+    var correctAnswer = current_question.answerChoices[correctIndex];
+    // console.log(correctAnswer); // works!
+
+    // console.log(current_question.answerChoices[current_question.correcAnswer]); // doesn't work!!
+    // var correct_answer = current_question.answerChoices[current_question.correcAnswer];
+    // console.log(correct_answer);
+
+    // 3) check if the user was correct:
+    if (current_question.correctAnswer == this.user_guess){
+      // display that the user was correct & the correct answer
+      // console.log("You are correct");
+      var message = $("<div>").append(
+                        $("<p>").html(correctAnswer + " is right!")
+                      );
+      this.correctAnswers ++;
+    } else {
+      // display user was wrong, and the correct answer
+      // console.log("You're wrong");
+      var message = $("<div>").append(
+                      $("<p>").html("Sorry, " + correctAnswer +
+                        " was the correct answer.")
+                      );
+      this.incorrectAnswers ++;
+    }
+
+    // 4) Update the view of #update-target ...
+    $("#update-target").append(message);
+    // check if the user's choice was correct or not
+    // create a set time out interval that will display time ...
+    this.intervalTimer = setTimeout(function(){
+      // empty the html div
+      console.log("TADA!");
+      $("#update-target").empty();
+      // set the index to the next potential question in array, and display it
+      this.currentIndex ++;
+      this.nextQuestion(this.currentIndex);
+    }.bind(this), 1500)
+  },
+
+  gameOver: function(){
+    // create the message
+    var message = $("<h2>").html("You Finished the Game!").append(
+                    $("<h4>").html("Questions answered correctly: "
+                      + this.correctAnswers).append(
+                        $("<h4>").html("Questions answered incorrectly: "
+                          + this.incorrectAnswers)
+                      )
+                  );
+    // create a button for play again
+    var playAgain = $("<div>").append(
+                        $("<button>").addClass("btn btn-info btn-play-again").html(
+                          "play again?"
+                        )
+                    );
+    // append the button to the message
+    message.append(playAgain);
+    $("#update-target").empty().append(message);
+
+    // create a button that is an event listener for initialize game!
+    // the event listener has to be outside
+    var self = this;
+    $(".btn-play-again").on("click", function(){
+      // alert("another game!");
+      self.initialize();
+      // console.log("self: ");
+      // console.log(self);
+      // console.log("this: ");
+      // console.log(this);
+    });
+  },
+}
